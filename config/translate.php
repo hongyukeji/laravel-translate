0f5f901268d2324e7be3a596b1cd3bf029f6f203
shadow <admin@hongyuvip.com>
2019-05-23 00:08:37 +0800

    Initial commit

--- config/translate.php
+++ config/translate.php
@@ -56,7 +56,7 @@ return [
     /*
      * This is the translator used to translate the source language files. You can also specify your own here if you wish. It has to implement \Ben182\AutoTranslate\Translators\TranslatorInterface.
      */
-    'translator' => \Hongyukeji\LaravelTranslate\Translators\SimpleGoogleTranslator::class,
+    'translator' => \Hongyukeji\LaravelTranslate\Translators\YouDaoTranslator::class,
 
     'simple_google_translator' => [
 
