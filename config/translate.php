dcfed752e414b8b608a0bf934110f5b39c28fb6a
shadow <admin@hongyuvip.com>
2019-05-22 23:42:14 +0800

    Initial commit

--- config/translate.php
+++ config/translate.php
@@ -2,82 +2,37 @@
 
 return [
     /*
-    |--------------------------------------------------------------------------
-    | Translation Driver
-    |--------------------------------------------------------------------------
-    |
-    | This option controls which service you would like to use to obtain
-    | translations from. You may set this to one of the options below.
-    |
-    | Supported: "google"
-    |
-    */
-    'driver' => 'baidu',
+     * Here you can specify the source language code.
+     */
+    'source_language' => 'en',
 
     /*
-    |--------------------------------------------------------------------------
-    | Translation Service Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you can define all of the services which can be used for translation
-    | along with their associated settings. These settings will be passed
-    | to the translation service as per their own requirements.
-    |
-    */
-    'services' => [
-        'google' => [
-            'key' => env('GOOGLE_TRANSLATE_API_KEY')
-        ],
-        'baidu' => [
-            'appid' => env('BAIDU_TRANSLATE_API_APPID'),
-            'key'   => env('BAIDU_TRANSLATE_API_KEY')
-        ],
-        'youdao' => [
-            'appid' => env('YOUDAO_TRANSLATE_API_APPID'),
-            'key'   => env('YOUDAO_TRANSLATE_API_KEY')
-        ]
+     * Here you can specify the target language code(s). This can be a string or an array.
+     */
+    'target_language' => ['de'],
+
+    /*
+     * Specify the path to the translation files.
+     */
+    'path' => realpath(base_path('resources/lang')),
+
+    /*
+     * This is the translator used to translate the source language files. You can also specify your own here if you wish. It has to implement \Ben182\AutoTranslate\Translators\TranslatorInterface.
+     */
+    'translator' => \Hongyukeji\LaravelTranslate\Translators\SimpleGoogleTranslator::class,
+
+    'simple_google_translator' => [
+
+        // The translator will wait between these numbers between each request.
+        'sleep_between_requests' => [1, 3],
+
+        // If you want to proxy the requests, you can specify a proxy server here.
+        'proxy' => '',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Translation methods
-    |--------------------------------------------------------------------------
-    |
-    | Here you can define the methods which are used to provide translation
-    | lookups. We'll scan the the specified directories for these methods
-    | and translate their strings into the specified languages.
-    |
-    */
-    'methods' => [
-        'trans',
-        '__'
-    ],
+    'deepl' => [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Translation paths
-    |--------------------------------------------------------------------------
-    |
-    | Here you can define the paths which should be scanned to search
-    | for the translation methods. Sensible defaults are provided.
-    |
-    */
-    'paths' => [
-        app_path(),
-        resource_path()
+        // Your DeepL API Key. See https://www.deepl.com/pro.html#developer
+        'api_key' => '',
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Target languages
-    |--------------------------------------------------------------------------
-    |
-    | Which languages would you like to translate your application into?
-    | Languages must be specified as ISO 639-1 codes. For example
-    | English = en, French = fr
-    |
-    */
-    'targetLanguages' => [
-        'fr'
-    ]
 ];
--- src/TranslateServiceProvider.php
+++ src/TranslateServiceProvider.php
@@ -16,10 +16,12 @@ class TranslateServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([
+            /*$this->publishes([
                 __DIR__.'/../config/config.php' => config_path('translate.php'),
-            ], 'config');
-
+            ], 'config');*/
+            $this->publishes([
+                __DIR__ . '/../config/translate.php' => config_path('translate.php'),
+            ]);
             // Registering package commands.
             $this->commands([
                 AllCommand::class,
@@ -34,14 +36,14 @@ class TranslateServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'auto-translate');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'translate');
 
-        $this->app->bind(TranslatorInterface::class, config('auto-translate.translator'));
+        $this->app->bind(TranslatorInterface::class, config('translate.translator'));
 
         // Register the main class to use with the facade
-        $this->app->singleton('auto-translate', function () {
+        $this->app->singleton('translate', function () {
             config([
-                'langman.path' => config('auto-translate.path'),
+                'langman.path' => config('translate.path'),
             ]);
 
             return new Translate(app(Manager::class), app(TranslatorInterface::class));
